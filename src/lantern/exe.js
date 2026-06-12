590a15270c98ab2df3c944275d2b2e5919b4db72
Phil Riley <rileypb@gmail.com>
2026-06-12 13:27:51 -0400

    add some lantern npm commands

---
+++ src/lantern/exe.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const { execFileSync } = require("child_process");
+const path = require("path");
+
+const inputArg = process.argv[2];
+if (!inputArg) {
+    console.error("Usage: lantern-exe <input.lamp>");
+    process.exit(1);
+}
+
+const inputFile = path.resolve(inputArg);
+const buildDir = path.join(__dirname, "..", "..", "build");
+const tmpFile = path.join(buildDir, `${path.basename(inputFile, ".lamp")}.generated.js`);
+const lanternCli = path.join(__dirname, "index.js");
+
+execFileSync("node", [lanternCli, inputFile, tmpFile], { stdio: "inherit" });
+execFileSync("node", [tmpFile], { stdio: "inherit" });
